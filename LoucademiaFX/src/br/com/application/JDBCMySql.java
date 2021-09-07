662e7c1939790caae2b32d8f93df9208a3718d10
BrunoO <brunojo.orlandy@hotmail.com>
2021-09-07 17:12:17 -0300

    Configuração do banco, script para criar tabelas

--- LoucademiaFX/src/br/com/application/JDBCMySql.java
+++ LoucademiaFX/src/br/com/application/JDBCMySql.java
@@ -2,61 +2,130 @@ package br.com.application;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.swing.JOptionPane;
+
+import br.com.application.utils.LeitorScript;
 
 public class JDBCMySql {
 
-	static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
-	static final String DB_URL = "jdbc:mysql://localhost/CODINGGROUND";
+	private static final Logger logger = Logger.getLogger(JDBCMySql.class.getName());
+	private static final String DB_DRIVER = "com.mysql.cj.jdbc.Driver";
+	private static final String DB_CONNECTION = "jdbc:mysql://localhost:3306/loucademia?useTimezone=true&serverTimezone=UTC";
+	private static final String DB_USER = "root";
+	private static final String DB_PASSWORD = "1234";
+	static final String LABEL_CONEXAO = "Conexão";
+	private static final String PATH_SCRIPT = "./script/script.sql";
 
-	static final String USER = "root";
-	static final String PASS = "root";
+	private Connection conn;
 
-	public static void main(String[] args) {
+	public Connection testaConexao() throws SQLException {
 		Connection conn = null;
-		Statement stmt = null;
-
 		try {
-			Class.forName("com.mysql.jdbc.Driver");
-			System.out.println("Connecting to database...");
-			conn = DriverManager.getConnection(DB_URL, USER, PASS);
-			System.out.println("Creating statement...");
-			stmt = conn.createStatement();
-			String sql;
-			sql = "select * from users";
-			ResultSet rs = stmt.executeQuery(sql);
-			while (rs.next()) {
-				int id = rs.getInt("id");
-				int age = rs.getInt("age");
-				String name = rs.getString("name");
-				String sex = rs.getString("sex");
+			Class.forName(DB_DRIVER);
+			conn = DriverManager.getConnection(DB_CONNECTION, DB_USER, DB_PASSWORD);
+			conn.setAutoCommit(false);
 
-				System.out.print("ID: " + id);
-				System.out.print(", Name: " + name);
-				System.out.print(", Age: " + age);
-				System.out.println(", Sex: " + sex);
+			if (conn != null) {
+				JOptionPane.showMessageDialog(null, "Sucesso", LABEL_CONEXAO, JOptionPane.INFORMATION_MESSAGE);
 			}
-			rs.close();
-			stmt.close();
-			conn.close();
+
+			return conn;
+
+		} catch (ClassNotFoundException e) {
+			JOptionPane.showMessageDialog(null, "falhou", LABEL_CONEXAO, JOptionPane.WARNING_MESSAGE);
+			System.exit(0);
+			e.printStackTrace();
 		} catch (SQLException e) {
 			e.printStackTrace();
+		}
+		return conn;
+	}
+
+	public static Connection abreConexao() throws SQLException {
+		Connection connection = null;
+
+		try {
+			Class.forName(DB_DRIVER);
+		} catch (ClassNotFoundException exception) {
+			logger.log(Level.SEVERE, exception.getMessage());
+		}
+
+		try {
+			connection = DriverManager.getConnection(DB_CONNECTION, DB_USER, DB_PASSWORD);
+			return connection;
+		} catch (SQLException exception) {
+			logger.log(Level.SEVERE, exception.getMessage());
+		}
+
+		return connection;
+	}
+
+	public void criaDataBase() {
+		Connection conn = null;
+		try {
+			Class.forName(DB_DRIVER);
+			conn = DriverManager.getConnection(DB_CONNECTION, DB_USER, DB_PASSWORD);
+			conn.setAutoCommit(false);
+
+			if (conn != null) {
+				rodarScript(conn);
+				JOptionPane.showMessageDialog(null, "Tabelas Criadas com sucesso!", LABEL_CONEXAO,
+						JOptionPane.INFORMATION_MESSAGE);
+			}
+
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void rodarScript(Connection conn) throws SQLException {
+		PreparedStatement stmt = null;
+		String script = new String();
+		try {
+
+			script = LeitorScript.getConteudoArquivo(PATH_SCRIPT);
+			stmt = conn.prepareStatement(script);
+			stmt.executeUpdate();
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
-			try {
-				if (stmt != null)
-					stmt.close();
-			} catch (SQLException e2) {
+			closeAll(conn, stmt);
+		}
+
+	}
+
+	public static void closeAll(Connection conn) {
+		try {
+			if (conn != null) {
+				conn.close();
+				System.err.println("Fechada a conexão com banco de dados! ");
 			}
-			try {
-				if (conn != null)
-					conn.close();
-			} catch (SQLException se) {
-				se.printStackTrace();
+		} catch (Exception e) {
+			System.err.println("Não foi possivel fechar a conexão com o banco de dados!");
+			e.printStackTrace();
+		}
+	}
+
+	public static void closeAll(Connection conn, Statement stmt) {
+		try {
+			if (conn != null) {
+				closeAll(conn);
 			}
+			if (stmt != null) {
+				stmt.close();
+			}
+		} catch (Exception e) {
+			System.err.println("Não foi possivel fechar o statement!");
+			e.printStackTrace();
 		}
 	}
 
