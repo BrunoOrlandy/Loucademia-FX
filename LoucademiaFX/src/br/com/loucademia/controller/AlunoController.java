bbf2e04a3caac620bf4414ee8f4df15f9e21bce7
StudentUCS <65573383+StudentUCS@users.noreply.github.com>
2021-10-12 16:08:01 -0300

    Update AlunoController.java

--- LoucademiaFX/src/br/com/loucademia/controller/AlunoController.java
+++ LoucademiaFX/src/br/com/loucademia/controller/AlunoController.java
@@ -3,11 +3,17 @@ package br.com.loucademia.controller;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 
 public class AlunoController {
+	
+
+    @FXML
+    private TextField idNome;
 
 	@FXML
-	protected void btnCancelarAction(ActionEvent eventC) {
+	protected void btnVoltar(ActionEvent eventC) {
+		System.out.println("Voltar");
 		Platform.exit();
 	}
 
