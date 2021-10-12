57f5140aa4529871f6c482d31106aa55168ae307
StudentUCS <65573383+StudentUCS@users.noreply.github.com>
2021-10-12 17:32:26 -0300

    Update AlunoController.java

--- LoucademiaFX/src/br/com/loucademia/controller/AlunoController.java
+++ LoucademiaFX/src/br/com/loucademia/controller/AlunoController.java
@@ -3,13 +3,43 @@ package br.com.loucademia.controller;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
 
-public class AlunoController {
-	
+public class AlunoController {	
+
+	@FXML
+    private TextField cep;
 
     @FXML
-    private TextField idNome;
+    private TextField cidade;
+
+    @FXML
+    private TextField complemento;
+
+    @FXML
+    private DatePicker dataDeNascimento;
+
+    @FXML
+    private TextField estado;
+
+    @FXML
+    private TextField nome;
+
+    @FXML
+    private TextField numero;
+
+    @FXML
+    private TextField rg;
+
+    @FXML
+    private TextField rua;
+
+    @FXML
+    private TextField telefoneCelular;
+
+    @FXML
+    private TextField telefoneFixo;
 
 	@FXML
 	protected void btnVoltar(ActionEvent eventC) {
