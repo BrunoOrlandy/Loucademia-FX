01b5f6ad0ddca0ddd4a9301569ccd4e3d08c7637
StudentUCS <65573383+StudentUCS@users.noreply.github.com>
2021-10-12 21:06:25 -0300

    Update AlunoController.java

--- LoucademiaFX/src/br/com/loucademia/controller/AlunoController.java
+++ LoucademiaFX/src/br/com/loucademia/controller/AlunoController.java
@@ -1,13 +1,17 @@
 package br.com.loucademia.controller;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
-public class AlunoController {	
+public class AlunoController implements Initializable{	
 	
     @FXML
     private TextField nome;
@@ -42,13 +46,142 @@ public class AlunoController {
     @FXML
     private TextField telefoneFixo;
     
+    Label lblNome = new Label("Nome");
+    TextField tfNome = new TextField();
+        
+    Label lblContract = new Label("Contract");
+    TextField tfContract = new TextField();
     
-//    Label lblNome = new Label("Nome");
-//    TextField tfNome = new TextField();
-//
-//    Label lblContract = new Label("Contract");
-//    TextField tfContract = new TextField();
     
+    private void handleSubmitButtonAction(ActionEvent event)
+    {
+    	if(nome.getText().length() == 0)
+    	{	
+    		nome.setStyle("-fx-border-colro: red ; -fx-border-width: 2px;");
+    		//new animatefx.animation.Shake(nome).play();
+    	}
+    }
+    
+
+	public TextField getNome() {
+		return nome;
+	}
+
+	public void setNome(TextField nome) {
+		this.nome = nome;
+	}
+
+	public DatePicker getDataDeNascimento() {
+		return dataDeNascimento;
+	}
+
+	public void setDataDeNascimento(DatePicker dataDeNascimento) {
+		this.dataDeNascimento = dataDeNascimento;
+	}
+
+	public TextField getRg() {
+		return rg;
+	}
+
+	public void setRg(TextField rg) {
+		this.rg = rg;
+	}
+
+	public TextField getRua() {
+		return rua;
+	}
+
+	public void setRua(TextField rua) {
+		this.rua = rua;
+	}
+
+	public TextField getNumero() {
+		return numero;
+	}
+
+	public void setNumero(TextField numero) {
+		this.numero = numero;
+	}
+
+	public TextField getEstado() {
+		return estado;
+	}
+
+	public void setEstado(TextField estado) {
+		this.estado = estado;
+	}
+
+	public TextField getCidade() {
+		return cidade;
+	}
+
+	public void setCidade(TextField cidade) {
+		this.cidade = cidade;
+	}
+
+	public TextField getComplemento() {
+		return complemento;
+	}
+
+	public void setComplemento(TextField complemento) {
+		this.complemento = complemento;
+	}
+
+	public TextField getCep() {
+		return cep;
+	}
+
+	public void setCep(TextField cep) {
+		this.cep = cep;
+	}
+
+	public TextField getTelefoneCelular() {
+		return telefoneCelular;
+	}
+
+	public void setTelefoneCelular(TextField telefoneCelular) {
+		this.telefoneCelular = telefoneCelular;
+	}
+
+	public TextField getTelefoneFixo() {
+		return telefoneFixo;
+	}
+
+	public void setTelefoneFixo(TextField telefoneFixo) {
+		this.telefoneFixo = telefoneFixo;
+	}
+
+	public Label getLblNome() {
+		return lblNome;
+	}
+
+	public void setLblNome(Label lblNome) {
+		this.lblNome = lblNome;
+	}
+
+	public TextField getTfNome() {
+		return tfNome;
+	}
+
+	public void setTfNome(TextField tfNome) {
+		this.tfNome = tfNome;
+	}
+
+	public Label getLblContract() {
+		return lblContract;
+	}
+
+	public void setLblContract(Label lblContract) {
+		this.lblContract = lblContract;
+	}
+
+	public TextField getTfContract() {
+		return tfContract;
+	}
+
+	public void setTfContract(TextField tfContract) {
+		this.tfContract = tfContract;
+	}
 
 	@FXML
 	protected void btnVoltar(ActionEvent eventC) {
@@ -59,5 +192,11 @@ public class AlunoController {
 	@FXML
 	protected void btnSalvarAction(ActionEvent eventC) {
 		System.out.println("Registro Salvo");
+	}
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		// TODO Auto-generated method stub
+		
 	}
 }
