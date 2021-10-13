0c3b78dd51960a6d2d4eadc762b811ffc6892943
StudentUCS <65573383+StudentUCS@users.noreply.github.com>
2021-10-12 22:03:44 -0300

    Update LoginController.java

--- LoucademiaFX/src/br/com/loucademia/controller/LoginController.java
+++ LoucademiaFX/src/br/com/loucademia/controller/LoginController.java
@@ -5,11 +5,30 @@ import br.com.loucademia.startUp.StatUp;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 
 public class LoginController {
+	
+	
+    @FXML
+    private TextField login;
+
+    @FXML
+    private TextField senha;
+    
+	
 	@FXML
 	protected void btnLogarAction(ActionEvent event) {
-		StatUp.changeScreen(NomeTelaEnum.MENU);
+					
+    	if(login.getText().length() == 0)
+    	{	
+    		login.setStyle("-fx-border-color: red ; -fx-border-width: 1px;");
+    	} else if(senha.getText().length() == 0) {	
+    		senha.setStyle("-fx-border-color: red ; -fx-border-width: 1px;");
+    	} else {
+    		StatUp.changeScreen(NomeTelaEnum.MENU);
+    	}
+		
 		System.out.println("Logar");
 	}
 
